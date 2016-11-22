e87b7438a87dfa6d159cb78b9456026edfac72a6
Billy Clark <billy_clark@sil.org>
2016-11-22 13:44:50 -0500

    wrong favicon showing

--- themes/material/common-head-elements.php
+++ themes/material/common-head-elements.php
@@ -2,6 +2,9 @@
 <meta name="robots" content="noindex, nofollow"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<link rel="shortcut icon" href="/module.php/theme-material/favicon.ico" />
+
 <link rel="stylesheet" href="/module.php/theme-material/material.indigo-pink.1.2.1.min.css">
 <link rel="stylesheet" href="/module.php/theme-material/styles.css">
+
 <script async src="/module.php/theme-material/material.1.2.1.min.js"></script>
