542622ab70af1f275973e18894df6e2df424519f
Billy Clark <billy_clark@sil.org>
2017-06-23 12:54:34 -0400

    form was not submitting on IE.

--- themes/material/core/loginuserpass.php
+++ themes/material/core/loginuserpass.php
@@ -45,80 +45,79 @@
     <main class="mdl-layout__content" layout-children="column" child-spacing="center">
         <?php include __DIR__ . '/../common-announcement.php' ?>
 
-        <div class="mdl-card mdl-shadow--8dp">
-            <div class="mdl-card__media white-bg margin" layout-children="column">
-<!--                TODO: update to /logo.png-->
-                <img src="https://static.gtis.guru/idp-logo/sil-logo.png"
-                     alt="<?= $this->t('{material:login:logo}', ['{idpName}' => $idpName]) ?>">
-            </div>
+        <form method="POST" action="<?= htmlentities($_SERVER['PHP_SELF']) ?>">
+            <input type="hidden" name="AuthState"
+                   value="<?= htmlspecialchars($this->data['stateparams']['AuthState']) ?>" />
+            <input type="hidden" name="csrf-token" value="<?= htmlentities($csrfToken); ?>" />
 
-
-            <div class="mdl-card__title center">
-                <h1 class="mdl-card__title-text">
-                    <?= $this->t('{material:login:header}', ['{idpName}' => $idpName]) ?>
-                </h1>
-            </div>
-
-            <form method="POST" action="<?= htmlentities($_SERVER['PHP_SELF']) ?>"
-                  layout-children="column" class="mdl-card__supporting-text" id="loginform">
-
-                <input type="hidden" name="AuthState"
-                       value="<?= htmlspecialchars($this->data['stateparams']['AuthState']) ?>" />
-                <input type="hidden" name="csrf-token" value="<?= htmlentities($csrfToken); ?>" />
-
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <label for="username" class="mdl-textfield__label">
-                        <?= $this->t('{material:login:label_username}') ?>
-                    </label>
-                    <input type="text" name="username" class="mdl-textfield__input"
-                           value="<?= htmlspecialchars($username) ?>"
-                           <?= empty($username) ? 'autofocus' : '' ?> id="username"/>
+            <div class="mdl-card mdl-shadow--8dp">
+<!--TODO: look at filling viewport on phones.-->
+                <div class="mdl-card__media white-bg margin" layout-children="column">
+<!--TODO: update to /logo.png when done testing-->
+                    <img src="https://static.gtis.guru/idp-logo/sil-logo.png"
+                         alt="<?= $this->t('{material:login:logo}', ['{idpName}' => $idpName]) ?>">
                 </div>
 
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <label for="password" class="mdl-textfield__label">
-                        <?= $this->t('{material:login:label_password}') ?>
-                    </label>
-                    <input type="password" name="password" class="mdl-textfield__input"
-                           <?= ! empty($username) ? 'autofocus' : '' ?> id="password"/>
+                <div class="mdl-card__title center">
+                    <h1 class="mdl-card__title-text">
+                        <?= $this->t('{material:login:header}', ['{idpName}' => $idpName]) ?>
+                    </h1>
+                </div>
+
+                <div class="mdl-card__supporting-text" layout-children="column">
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                        <label for="username" class="mdl-textfield__label">
+                            <?= $this->t('{material:login:label_username}') ?>
+                        </label>
+                        <input type="text" name="username" class="mdl-textfield__input"
+                               value="<?= htmlspecialchars($username) ?>"
+                               <?= empty($username) ? 'autofocus' : '' ?> id="username"/>
+                    </div>
+
+                    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                        <label for="password" class="mdl-textfield__label">
+                            <?= $this->t('{material:login:label_password}') ?>
+                        </label>
+                        <input type="password" name="password" class="mdl-textfield__input"
+                               <?= ! empty($username) ? 'autofocus' : '' ?> id="password"/>
+                    </div>
                 </div>
 
                 <?php
                 if ($errorCode == 'WRONGUSERPASS') {
                 ?>
-                    <p class="mdl-card__supporting-text mdl-color-text--red error">
-                        <i class="material-icons margin">error</i>
+                    <p class="mdl-color-text--red error">
+                        <i class="material-icons">error</i>
 
                         <span class="mdl-textfield mdl-typography--caption">
-                            <?= $this->t($errorMessageKey, $errorMessageTokens) ?>
+                                <?= $this->t($errorMessageKey, $errorMessageTokens) ?>
                         </span>
                     </p>
                 <?php
                 }
                 ?>
-            </form>
 
-            <div class="mdl-card__actions" layout-children="row">
-                <?php
-                $forgotPasswordUrl = "asfsgf";
-                if (! empty($forgotPasswordUrl)) {
-                ?>
-                <a href="<?= htmlentities($forgotPasswordUrl) ?>" target="_blank"
-                   class="mdl-button mdl-button--colored mdl-typography--caption">
-                    <?= $this->t('{material:login:forgot}') ?>
-                </a>
-                <?php
-                }
-                ?>
-<!--TODO: look at filling viewport on phones.-->
-                <span flex></span>
+                <div class="mdl-card__actions" layout-children="row">
+                    <?php
+                    $forgotPasswordUrl = "asfsgf"; //TODO: remove when done testing.
+                    if (! empty($forgotPasswordUrl)) {
+                    ?>
+                    <a href="<?= htmlentities($forgotPasswordUrl) ?>" target="_blank"
+                       class="mdl-button mdl-button--colored mdl-typography--caption">
+                        <?= $this->t('{material:login:forgot}') ?>
+                    </a>
+                    <?php
+                    }
+                    ?>
 
-                <button type="submit" form="loginform"
-                        class="mdl-button mdl-button--colored mdl-button--raised">
-                    <?= $this->t('{material:login:button_login}') ?>
-                </button>
+                    <span flex></span>
+
+                    <button class="mdl-button mdl-button--colored mdl-button--raised">
+                        <?= $this->t('{material:login:button_login}') ?>
+                    </button>
+                </div>
             </div>
-        </div>
+        </form>
     </main>
 </div>
 </body>
