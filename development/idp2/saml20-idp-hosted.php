17eb5ab7dc19163b9e32d84191860fe8f213f05b
Billy Clark <billy_clark@sil.org>
2017-07-19 08:45:08 -0400

    not needed

--- development/idp2/saml20-idp-hosted.php
+++ development/idp2/saml20-idp-hosted.php
@@ -5,7 +5,6 @@ use Sil\Psr3Adapters\Psr3SamlLogger;
 $metadata['http://ssp-hub-idp2.local:8086'] = [
 	'host' => '__DEFAULT__',
 	'privatekey' => 'ssp-hub-idp2.pem',
-	'certificate' => 'ssp-hub-idp2.crt',
 	'auth' => 'example-userpass',
     'authproc' => [
         10 => [
