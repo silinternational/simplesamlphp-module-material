0d42202d5d562812587a93e4287d3b06e25cdfc8
Billy Clark <billy_clark@sil.org>
2017-08-30 14:12:58 -0400

    non-trivial HTML was not rendering properly in a `<p>`

--- themes/material/common-announcement.php
+++ themes/material/common-announcement.php
@@ -3,9 +3,9 @@ $announcementHtml = $this->configuration->getValue('announcement');
 
 if (! empty($announcementHtml)) {
 ?>
-  <p class="mdl-typography--subhead mdl-typography--text-center alert margin">
+  <div class="mdl-typography--subhead mdl-typography--text-center alert margin">
       <?= $announcementHtml ?>
-  </p>
+  </div>
 <?php
 }
 ?>
