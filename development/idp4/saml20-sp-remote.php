9b0dd72fee0fa0fc97abdbf94c0f58267b038cc2
Billy Clark <billy_clark@sil.org>
2017-11-01 14:58:06 -0400

    being directed to wrong page after successful authn

--- development/idp4/saml20-sp-remote.php
+++ development/idp4/saml20-sp-remote.php
@@ -1,5 +1,5 @@
 <?php
 $metadata['ssp-hub.local'] = [
-	'AssertionConsumerService' => 'http://ssp-hub.local:8080/module.php/saml/sp/saml2-acs.php/hub-discovery',
-	'SingleLogoutService' => 'http://ssp-hub.local:8080/module.php/saml/sp/saml2-logout.php/hub-discovery',
+	'AssertionConsumerService' => 'http://ssp-hub.local/module.php/saml/sp/saml2-acs.php/hub-discovery',
+	'SingleLogoutService' => 'http://ssp-hub.local/module.php/saml/sp/saml2-logout.php/hub-discovery',
 ];
