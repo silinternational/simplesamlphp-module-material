a6e9ecc8962bee4c178f04be8efe164ede1d9553
Billy Clark <billy_clark@sil.org>
2017-12-11 12:01:17 -0500

    logos on disabled cards for the hub were to small

--- themes/material/mfa/prompt-for-mfa-backupcode.php
+++ themes/material/mfa/prompt-for-mfa-backupcode.php
@@ -18,7 +18,7 @@
         <form layout-children="column" method="post" autocomplete="off">
             <div class="mdl-card mdl-shadow--8dp">
                 <div class="mdl-card__media white-bg margin" layout-children="column">
-                    <img src="mfa-backupcode.svg"
+                    <img src="mfa-backupcode.svg" class="icon"
                          alt="<?= $this->t('{material:mfa:backupcode_icon}') ?>">
                 </div>
 
